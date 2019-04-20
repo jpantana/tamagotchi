2e2aa9934e36e3a4f35e52f2632f5c51cdd1d435
JPantana <joshpantana@me.com>
2019-04-20 12:13:21 -0500

    got play quad working

--- src/javascripts/components/play/play.js
+++ src/javascripts/components/play/play.js
@@ -1,6 +1,6 @@
 import util from '../../helpers/util';
 
-let total = 100;
+let total = 0;
 
 const fun = (num) => {
   total += num;
@@ -17,8 +17,8 @@ const domStringBuilder = () => {
   domString += '  <h1>Eat Quad</h1>';
   domString += '  <p>jpan</p>';
   domString += `  <p>Fullness Score: <span>${total}</span></p>`;
-  domString += '  <button type="submit" id="superFun">Pure Lemon Grass Seed Oil Extract</button>';
-  domString += '  <button type="submit" id="slightlyFun">Cheeseburger Pizza</button>';
+  domString += '  <button type="submit" id="superFun">Taking A European Vacation</button>';
+  domString += '  <button type="submit" id="slightlyFun">Coding Homework</button>';
   domString += '</div>';
   util.printToDom('play', domString);
   document.getElementById('superFun').addEventListener('click', (e) => {
