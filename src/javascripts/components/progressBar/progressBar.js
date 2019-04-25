22f77b8eb0b2128929afa28c87c428c4848a38d5
JPantana <joshpantana@me.com>
2019-04-25 18:36:20 -0500

    progress bar mostly works

--- src/javascripts/components/progressBar/progressBar.js
+++ src/javascripts/components/progressBar/progressBar.js
@@ -6,33 +6,24 @@ const newArr = [];
 
 const progressBar = (num) => {
   let domString = '';
-  console.error(num);
   if (num !== undefined) {
     thisNum = num + 300;
+    if (thisNum > 400) {
+      thisNum = 400;
+    } else if (thisNum < 0) {
+      thisNum = 0;
+    }
     console.error(thisNum);
   }
-  if (thisNum >= 400) {
-    thisNum = 400;
-    // console.error(thisNum);
-  } else if (thisNum <= 0) {
-    thisNum = 0;
-    // console.error(thisNum);
-  }
-  domString += '<h3 id="totalH1">HEALTH</h3>';
-  domString += `<progress style="background-color:#FF0000;" id="progressbar" value="${thisNum}" max="400"></progress>`;
-  domString += `<p id="totalNumPrint">${thisNum}</p>`;
+  domString += '  <h3 id="totalH1">HEALTH</h3>';
+  domString += `  <progress style="background-color: #FF0000;" id="progressbar" value="${thisNum}" max="400"></progress>`;
+  domString += `  <p id="totalNumPrint">${thisNum}</p>`;
   util.printToDom('progress', domString);
 };
 
 const totalHealth = (x) => {
   newArr.push(x);
   thisNum = newArr.reduce((acc, b) => acc + b);
-  // if (thisNum >= 400) {
-  //   thisNum = 400;
-  //   console.error(thisNum);
-  // } else if (thisNum <= 0) {
-  //   thisNum = 0;
-  // }
   progressBar(thisNum);
 };
 
